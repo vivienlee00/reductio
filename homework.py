c92b94a8489dae7211d67b7953904042b510df20
vivienlee00 <vivien.lee.601@gmail.com>
2018-04-30 20:10:28 -0400

    done

--- homework.py
+++ homework.py
@@ -7,13 +7,13 @@ from functools import reduce
 
 #read text file, remove line breaks and empty strings
 tempest = open('thetempest.txt', 'r')
-tempest_lines = tempest.read().replace("\n", " ").split(" ")
-tempest_lines = filter(None, tempest_lines)
+tempest_words = tempest.read().replace("\n", " ").split(" ")
+tempest_words = filter(None, tempest_words)
 
 def word_frequency(word):
     #remove punctuation, uppercase letters
     #add 1 for each occurance of the word
-    count = [1 for x in tempest_lines if word.lower() == x.lower().strip("?!.,_[]")]
+    count = [1 for x in tempest_words if word.lower() == x.lower().strip("?!.,_[]")]
     if len(count) != 0:
         return reduce((lambda x, y: x + y), count)
     else:
@@ -22,8 +22,23 @@ def word_frequency(word):
 #print word_frequency("Prospero")
 
 def group_word_frequency(words):
+    #only works with lists !!
+    #find frequency for each word in list
     counts = [word_frequency(x) for x in words]
+
+    #sum counts for each word in list
     count = reduce((lambda x, y: x + y) , counts)
     return count
 
-print group_word_frequency(["Prospero", "hi", "hey"])
+#print group_word_frequency(["Prospero", "the", "yolo"])
+
+def most_frequent_word():
+
+    #creates a set of all words in The Tempest -- no repeating words
+    words = {x for x in tempest_words}
+    counts = [word_frequency(x) for x in words]
+    return max(counts)
+
+#print most_frequent_word()
+#takes very long.
+#it's "the".
